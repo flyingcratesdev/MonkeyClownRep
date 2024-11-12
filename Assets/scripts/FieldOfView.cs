21e2f6c9dde457b3e56159932cf15554b52c5fb9
flyingcratesdev <joeflydev@gmail.com>
2024-11-12 14:40:04 -0800

    What I have so far

---
+++ Assets/scripts/FieldOfView.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    [SerializeField]
+    private LayerMask layerMask;
+    private Mesh mesh;
+    float fov = 75f;
+    Vector3 origin;
+    [SerializeField]
+    private float startingAngle;
+    int rayCount = 40;
+    float viewDistance = 5f;
+
+    void Start()
+    {
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        origin = Vector3.zero;
+    }
+    
+
+
+    public Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+
+
+    }
+    public float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if(n < 0) n += 360;
+        return n;
+
+    }
+ 
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+            if (raycastHit2D.collider == null)
+            {
+
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+
+                vertex = raycastHit2D.point;
+
+            }
+            vertices[vertexIndex] = vertex;
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+            vertexIndex++;
+            angle -= angleIncrease;
+
+
+
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    public void SetOrigin(Vector3 neworigin)
+    {
+        this.origin = neworigin;
+    }
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        startingAngle = GetAngleFromVectorFloat(aimDirection) - fov/2f;
+
+
+    }
+}
--- Assets/scripts/enemyScript.cs
+++ Assets/scripts/enemyScript.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyScript : MonoBehaviour
 {
+    public FieldOfView view;
     public Transform[] patrolPoints;
-    public int enemyTarget;
+    public int enemyTarget = 0;
     public float moveSpeed;
-
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        enemyTarget = 0;
+        agent.updatePosition = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        view.SetOrigin(transform.position);
+        view.SetAimDirection(transform.up);
     }
 
     void moveEnemy()
